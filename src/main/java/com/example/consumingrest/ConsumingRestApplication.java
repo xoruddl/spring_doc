b829abc680c5bebf7e3f1df78e15540ef0ac838f
itaekyung <taeyun1411@gmail.com>
2026-08-05 12:21:49 +0900

    feat: Consuming a RESTful Web Service

---
+++ src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,34 @@
+package com.example.consumingrest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestClient;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class, args);
+    }
+
+    @Bean
+    @Profile("!test")
+    public ApplicationRunner run(RestClient.Builder builder) {
+        RestClient restClient = builder.baseUrl("http://localhost:8080").build();
+        return args -> {
+            Quote quote = restClient
+                    .get().uri("/api/random")
+                    .retrieve()
+                    .body(Quote.class);
+            log.info(quote.toString());
+        };
+    }
+}
